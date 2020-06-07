03a85960fbbbd5d6501743827f4fe15fbdd23c15
ArtemSharapanyk <artmencd2003@gmail.com>
2020-06-07 16:31:07 +0300

    created services, blog sctions with responsive

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import './assets/css/media/mainMedia.css';
 import './assets/css/header.css';
 import './assets/css/media/headerMedia.css';
 
+import './assets/css/services.css';
+import './assets/css/media/servicesMedia.css';
+
+import './assets/css/blog.css';
+import './assets/css/media/blogMedia.css'
+
 document.addEventListener('DOMContentLoaded', () => {
     let link = document.querySelectorAll('a');
     let menu = require('./js/menu');
